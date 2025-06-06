52a135b7ee8115e036bde35b020f8726dab63daf
Parshvi Jain <parshvijain1508@gmail.com>
2025-06-06 20:50:27 +0530

    Cleared the clutter

--- src/core/recommendations.py
+++
@@ -1,5 +0,0 @@
-# src/core/recommendations.py
-def recommend_restaurants(user_prefs):
-    return supabase_client.table('restaurants').select('*').ilike(
-        'cuisine', f"%{user_prefs['cuisine']}%"
-    ).lte('price_range', user_prefs['max_price']).execute()
